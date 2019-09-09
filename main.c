b48b10298222ec36f47139448bb55eabbde0620b
Sopaipa <Alejoo@localhost.localdomain>
2019-09-09 17:47:38 -0300

    c guardan los wns en el arreglo

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(void){
 
-  system("mkdir test");
+  char jugadores[4][10];
+  for (int i=0;i<4;i++){
+    printf("Jugador %d: ",i);
+    scanf("%s",&jugadores[i]);
+  }
+  for (int i=0;i<4;i++) printf("%s\n",jugadores[i]);
   return 0;
 }
