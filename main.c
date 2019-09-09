226665784ebe0a29972935301324fc72c9d6542b
Sopaipa <Alejoo@localhost.localdomain>
2019-09-09 18:06:58 -0300

    creando directorios para jugadores jeje

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 
 
 int main(void){
-
+  char *aux;
   char jugadores[4][10];
   for (int i=0;i<4;i++){
     printf("Jugador %d: ",i);
     scanf("%s",&jugadores[i]);
   }
-  for (int i=0;i<4;i++) printf("%s\n",jugadores[i]);
+  for (int i=0;i<4;i++) {
+    strcpy(aux,"mkdir ");
+    strcat(aux, jugadores[i]);
+    system(aux);
+  }
   return 0;
 }
